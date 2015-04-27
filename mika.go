98dc130d4aeafa3d5cf591116fbe59a040663271
Leigh MacDonald <leigh.macdonald@gmail.com>
2015-04-26 20:34:26 -0600

    Remove redis conn idle timeout

--- mika.go
+++ mika.go
@@ -84,7 +84,7 @@ var (
  ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯      / ========= === == / /   ////
                      <__________________<_/    ¯¯¯
 `
-	// Error code to message mappings
+// Error code to message mappings
 	resp_msg = map[int]string{
 		MSG_INVALID_REQ_TYPE:        "Invalid request type",
 		MSG_MISSING_INFO_HASH:       "info_hash missing from request",
@@ -104,14 +104,14 @@ var (
 
 	version string
 
-	// Channels
-	sync_user    = make(chan *User, 100)
-	sync_peer    = make(chan *Peer, 1000)
+// Channels
+	sync_user = make(chan *User, 100)
+	sync_peer = make(chan *Peer, 1000)
 	sync_torrent = make(chan *Torrent, 500)
-	counter      = make(chan int)
+	counter = make(chan int)
 
 	err_parse_reply = errors.New("Failed to parse reply")
-	err_cast_reply  = errors.New("Failed to cast reply into type")
+	err_cast_reply = errors.New("Failed to cast reply into type")
 
 	stats *StatsCounter
 
@@ -121,9 +121,9 @@ var (
 	config     *Config
 	configLock = new(sync.RWMutex)
 
-	profile     = flag.String("profile", "", "write cpu profile to file")
+	profile = flag.String("profile", "", "write cpu profile to file")
 	config_file = flag.String("config", "./config.json", "Config file path")
-	num_procs   = flag.Int("procs", runtime.NumCPU()-1, "Number of CPU cores to use (default: ($num_cores-1))")
+	num_procs = flag.Int("procs", runtime.NumCPU()-1, "Number of CPU cores to use (default: ($num_cores-1))")
 )
 
 // math.Max for uint64
@@ -235,7 +235,7 @@ func main() {
 	connDone = make(chan redis.Conn, 200)
 	pool = &redis.Pool{
 		MaxIdle:     100,
-		IdleTimeout: 240 * time.Second,
+		IdleTimeout: 0,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", config.RedisHost)
 			if err != nil {
@@ -249,15 +249,15 @@ func main() {
 			}
 			return c, err
 		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			if err != nil {
-				// TODO remove me, temp hack to allow supervisord to reload process
-				// since we currently don't acutually handle graceful reconnects yet.
-				log.Fatalln("Bad redis voodoo! exiting!", err)
-			}
-			return err
-		},
+		//		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+		//			_, err := c.Do("PING")
+		//			if err != nil {
+		//				// TODO remove me, temp hack to allow supervisord to reload process
+		//				// since we currently don't acutually handle graceful reconnects yet.
+		//				log.Fatalln("Bad redis voodoo! exiting!", err)
+		//			}
+		//			return err
+		//		},
 	}
 
 	// Initialize the redis pool manager
@@ -332,7 +332,7 @@ func init() {
 	go func() {
 		for received_signal := range s {
 			switch received_signal {
-			case syscall.SIGINT:
+				case syscall.SIGINT:
 				log.Println("\nShutting down!")
 				if *profile != "" {
 					log.Println("> Writing out profile info")
@@ -340,7 +340,7 @@ func init() {
 				}
 				CaptureMessage("Stopped tracker")
 				os.Exit(0)
-			case syscall.SIGUSR2:
+				case syscall.SIGUSR2:
 				log.Println("SIGUSR2")
 				<-s
 				loadConfig(false)

6481f292bc5e279c1b9d7e5d386d2d04456729cb
Leigh MacDonald <leigh.macdonald@gmail.com>
2015-05-02 15:35:12 -0600

    test for speed est

--- mika.go
+++ mika.go
@@ -190,7 +190,7 @@ func responseError(message string) string {
 
 // Estimate a peers speed using downloaded amount over time
 func estSpeed(start_time int32, last_time int32, bytes_sent uint64) float64 {
-	if start_time <= 0 || last_time <= 0 || bytes_sent <= 0 || last_time < start_time {
+	if start_time <= 0 || last_time <= 0 || bytes_sent == 0 || last_time < start_time {
 		return 0.0
 	}
 	return float64(bytes_sent) / (float64(last_time) - float64(start_time))
--- mika_test.go
+++ mika_test.go
@@ -21,3 +21,35 @@ func TestUMax(t *testing.T) {
 		t.Error("Invalid min value")
 	}
 }
+
+func TestEstSpeed(t *testing.T) {
+	a := estSpeed(1000, 2000, 0)
+	if a != 0.0 {
+		t.Errorf("A: Invalid value %f", a)
+	}
+
+	b := estSpeed(2000, 1000, 0)
+	if b != 0.0 {
+		t.Errorf("B: Invalid value %f", b)
+	}
+
+	c := estSpeed(-1, 1, 0)
+	if c != 0.0 {
+		t.Errorf("C: Invalid value %f", c)
+	}
+
+	d := estSpeed(1, -1, 0)
+	if d != 0.0 {
+		t.Errorf("D: Invalid value %f", d)
+	}
+
+	e := estSpeed(0, 0, 1)
+	if e != 0.0 {
+		t.Errorf("E: Invalid value %f", e)
+	}
+
+	ok := estSpeed(1000, 2000, 100000000)
+	if ok != 100000.0 {
+		t.Errorf("E: Invalid value %f", ok)
+	}
+}

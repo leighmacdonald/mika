39a878ef45fd305b9f81fe3cfc23ac29151e40ab
Leigh MacDonald <leigh.macdonald@gmail.com>
2020-04-26 05:10:49 -0600

    Updated copy for cli interface

--- cmd/client.go
+++ cmd/client.go
@@ -13,13 +13,8 @@ import (
 // clientCmd represents the client command
 var clientCmd = &cobra.Command{
 	Use:   "client",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "CLI to administer a running instance",
+	Long:  `CLI to administer a running instance`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("client called")
 	},

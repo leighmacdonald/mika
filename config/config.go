0d39e3111dd08641067b7969de25c81c40aa27f1
Leigh MacDonald <leigh.macdonald@gmail.com>
2020-05-01 02:14:46 -0600

    Doc strings added for all the config options

--- config/config.go
+++ config/config.go
@@ -23,7 +23,7 @@ const (
 )
 
 const (
-	// GeneralRunMode defines the aapplication run mode.
+	// GeneralRunMode defines the application run mode.
 	// debug|release
 	GeneralRunMode = "general_run_mode"
 
@@ -37,44 +37,114 @@ const (
 
 	// TrackerPublic enables/disables auto registration of torrents and users
 	// true|false
-	TrackerPublic              = "tracker_public"
-	TrackerListen              = "tracker_listen"
-	TrackerTLS                 = "tracker_tls"
-	TrackerIPv6                = "tracker_ipv6"
-	TrackerIPv6Only            = "tracker_ipv6_only"
-	TrackerAnnounceInterval    = "tracker_announce_interval"
+	TrackerPublic = "tracker_public"
+	// TrackerListen sets the host and port to listen on
+	// hostname:port
+	TrackerListen = "tracker_listen"
+	// TrackerTLS enables TLS for the tracker component
+	// true|false
+	TrackerTLS = "tracker_tls"
+	// TrackerIPv6 enables ipv6 peers
+	// true|false
+	TrackerIPv6 = "tracker_ipv6"
+	// TrackerIPv6Only disables ipv4 peers
+	// true|false
+	TrackerIPv6Only = "tracker_ipv6_only"
+	// TrackerAnnounceInterval defines how often peers should announce. The lower this is
+	// the more load on your system you can expect
+	// 60s|1m
+	TrackerAnnounceInterval = "tracker_announce_interval"
+	// TrackerAnnounceIntervalMin is the minimum interval a client is allowed
+	// 60s|1m
 	TrackerAnnounceIntervalMin = "tracker_announce_interval_minimum"
-	TrackerReapInterval        = "tracker_reap_internal"
-	TrackerHNRThreshold        = "tracker_hnr_threshold"
-	TrackerIndexInterval       = "tracker_index_interval"
+	// TrackerReapInterval defines how often we do a sweep of active swarms looking for stale
+	// peers that can be removed.
+	// 60s|1m
+	TrackerReapInterval = "tracker_reap_internal"
+	// TrackerHNRThreshold is how much time must pass before we mark a peer as Hit-N-Run
+	// 1d|12h|60m
+	TrackerHNRThreshold = "tracker_hnr_threshold"
+	// TrackerIndexInterval is the amount of time between updating the torrent stats
+	// 60s|1m
+	TrackerIndexInterval = "tracker_index_interval"
 
-	APIListen   = "api_listen"
-	APITLS      = "api_tls"
-	APIIPv6     = "api_ipv6"
+	// APIListen sets the host and port that the admin API should bind to
+	// localhost:34001
+	APIListen = "api_listen"
+	// APITLS enables TLS1.3 on the admin interface.
+	// true|false
+	APITLS = "api_tls"
+	// APIIPv6 enables ipv6 for the admin API
+	// true|false
+	APIIPv6 = "api_ipv6"
+	// APIIPv6Only disabled ipv4 to the admin interface
+	// true|false
 	APIIPv6Only = "api_ipv6_only"
 
-	StoreTorrentType       = "store_torrent_type"
-	StoreTorrentHost       = "store_torrent_host"
-	StoreTorrentPort       = "store_torrent_port"
-	StoreTorrentDatabase   = "store_torrent_database"
-	StoreTorrentUser       = "store_torrent_user"
-	StoreTorrentPassword   = "store_torrent_password"
+	// StoreTorrentType sets the backing store type to be used for torrents
+	// memory|redis|postgres|mysql|http
+	StoreTorrentType = "store_torrent_type"
+	// StoreTorrentHost is the host to connect to
+	// localhost
+	StoreTorrentHost = "store_torrent_host"
+	// StoreTorrentPort is the port to connect to
+	// 3306|6379|443
+	StoreTorrentPort = "store_torrent_port"
+	// StoreTorrentDatabase is the database / schema name to open on the backing store
+	// Redis uses numeric values 0-16 by default
+	// mika|0
+	StoreTorrentDatabase = "store_torrent_database"
+	// StoreTorrentUser user to connect with
+	// mika
+	StoreTorrentUser = "store_torrent_user"
+	// StoreTorrentPassword password to connect with
+	// mika
+	StoreTorrentPassword = "store_torrent_password"
+	// Additional properties passed to the backing store configuration
 	StoreTorrentProperties = "store_torrent_properties"
 
-	StoreUsersType       = "store_users_type"
-	StoreUsersHost       = "store_users_host"
-	StoreUsersPort       = "store_users_port"
-	StoreUsersDatabase   = "store_users_database"
-	StoreUsersUser       = "store_users_user"
-	StoreUsersPassword   = "store_users_password"
+	// StoreUsersType sets the backing store type to be used for users
+	// memory|redis|postgres|mysql|http
+	StoreUsersType = "store_users_type"
+	// StoreUsersHost is the host to connect to
+	// localhost
+	StoreUsersHost = "store_users_host"
+	// StoreUsersPort is the port to connect to
+	// 3306|6379|443
+	StoreUsersPort = "store_users_port"
+	// StoreUsersDatabase is the database / schema name to open on the backing store
+	// Redis uses numeric values 0-16 by default
+	// mika|0
+	StoreUsersDatabase = "store_users_database"
+	// StoreUsersUser user to connect with
+	// mika
+	StoreUsersUser = "store_users_user"
+	// StoreUsersPassword password to connect with
+	// mika
+	StoreUsersPassword = "store_users_password"
+	// StoreUsersProperties sets additional properties passed to the backing store configuration
 	StoreUsersProperties = "store_users_properties"
 
-	StorePeersType       = "store_peers_type"
-	StorePeersHost       = "store_peers_host"
-	StorePeersPort       = "store_peers_port"
-	StorePeersDatabase   = "store_peers_database"
-	StorePeersUser       = "store_peers_user"
-	StorePeersPassword   = "store_peers_password"
+	// StorePeersType sets the backing store type to be used for peers
+	// memory|redis|postgres|mysql|http
+	StorePeersType = "store_peers_type"
+	// StorePeersHost is the host to connect to
+	// localhost
+	StorePeersHost = "store_peers_host"
+	// StorePeersPort is the port to connect to
+	// 3306|6379|443
+	StorePeersPort = "store_peers_port"
+	// StorePeersDatabase is the database / schema name to open on the backing store
+	// Redis uses numeric values 0-16 by default
+	// mika|0
+	StorePeersDatabase = "store_peers_database"
+	// StorePeersUser user to connect with
+	// mika
+	StorePeersUser = "store_peers_user"
+	// StorePeersPassword password to connect with
+	// mika
+	StorePeersPassword = "store_peers_password"
+	// StorePeersProperties sets additional store specific properties passed to the backing store configuration
 	StorePeersProperties = "store_peers_properties"
 
 	// GeodbPath sets the path to use for downloading and loading the geo database. Relative to the binary's path.

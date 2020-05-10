a87e88c2386a9be232ac8068d55e043302dd54a7
Leigh MacDonald <leigh.macdonald@gmail.com>
2020-05-10 06:44:15 -0600

    Added NewPasskey func

---
+++ util/string.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const passkeyLen = 20
+
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func randStringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func NewPasskey() string {
+	return randStringWithCharset(passkeyLen, charset)
+}

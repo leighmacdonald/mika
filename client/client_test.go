f1fdc57b859aad21e99592e15e3bfa03b047a276
Leigh MacDonald <leigh.macdonald@gmail.com>
2020-05-13 03:25:46 -0600

    Start tracker api instance for testing

--- client/client_test.go
+++ client/client_test.go
@@ -1,15 +1,34 @@
 package client
 
 import (
+	"context"
+	"fmt"
 	"github.com/leighmacdonald/mika/config"
-	"github.com/spf13/viper"
+	h "github.com/leighmacdonald/mika/http"
+	"github.com/leighmacdonald/mika/model"
+	"github.com/leighmacdonald/mika/tracker"
 	"github.com/stretchr/testify/require"
 	"log"
+	"net/http"
 	"os"
 	"testing"
+	"time"
 )
 
-var host string
+var (
+	host   = "localhost:34000"
+	server *http.Server
+	ihStr  = "ff503e9ca036f1647c2dfc1337b163e2c54f13f8"
+)
+
+func TestClient_Torrent(t *testing.T) {
+	c := New(host)
+	var ih model.InfoHash
+	_ = model.InfoHashFromString(&ih, ihStr)
+	require.NoError(t, c.TorrentAdd(ih, "test torrent"))
+	require.NoError(t, c.TorrentDelete(ih))
+
+}
 
 func TestClient_Ping(t *testing.T) {
 	c := New(host)
@@ -17,9 +36,22 @@ func TestClient_Ping(t *testing.T) {
 }
 
 func TestMain(m *testing.M) {
+	ctx := context.Background()
 	if err := config.Read("mika_testing"); err != nil {
 		log.Fatalf("failed to read config")
 	}
-	host = viper.GetString(string(config.APIListen))
-	os.Exit(m.Run())
+	tkr, _, _, _ := tracker.NewTestTracker()
+	handler := h.NewAPIHandler(tkr)
+	server = h.CreateServer(handler, "localhost:34000", false)
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			fmt.Printf("Error serving test api server: %s", err.Error())
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	retVal := m.Run()
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Error shutting down test api server: %s", err.Error())
+	}
+	os.Exit(retVal)
 }
